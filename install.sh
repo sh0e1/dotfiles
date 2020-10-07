48b659f7478cb795aadf6153c5e9a323a89dfb0c
sh0e1 <sh0e1@users.noreply.github.com>
2020-10-08 00:19:37 +0900

    :+1: Update: install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@ set -e
 
 dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 
+# ${dir}/brew.sh
+# ${dir}/nvim.sh
+# ${dir}/tmux.sh
+
 for f in .??*; do
   [[ "$f" == ".git" ]] && continue
   [[ "$f" == ".DS_Store" ]] && continue
