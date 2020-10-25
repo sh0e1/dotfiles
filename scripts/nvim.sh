e9116e878343357cd3bd041da9cb48d85825dead
sh0e1 <sh0e1@users.noreply.github.com>
2020-10-25 19:03:30 +0900

    :+1: Update: scripts/nvim.sh

--- scripts/nvim.sh
+++ scripts/nvim.sh
@@ -6,7 +6,9 @@ dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > ${dir}/installer.sh
 sh ${dir}/installer.sh ~/.cache/dein
-
 pip3 install --user pynvim
-
 rm -f ${dir}/installer.sh
+
+# textlint
+npm install -g textlint
+npm install -g textlint-rule-preset-ja-technical-writing
