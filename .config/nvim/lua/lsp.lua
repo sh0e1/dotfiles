6c41278230041db97dbaa7534275fe3412434eca
sh0e1 <sh0e1@users.noreply.github.com>
2022-08-21 23:25:41 +0900

    :sparkles: Add: GoImports function

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -85,5 +85,20 @@ require('mason-lspconfig').setup_handlers({
   end,
 })
 
+function GoImports(wait_ms)
+  local params = vim.lsp.util.make_range_params()
+  params.context = {only = {'source.organizeImports'}}
+  local result = vim.lsp.buf_request_sync(0, 'textDocument/codeAction', params, wait_ms)
+  for cid, res in pairs(result or {}) do
+    for _, r in pairs(res.result or {}) do
+      if r.edit then
+        local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or 'utf-16'
+        vim.lsp.util.apply_workspace_edit(r.edit, enc)
+      end
+    end
+  end
+end
+vim.api.nvim_exec('autocmd BufWritePre *.go lua GoImports(1000)', false)
+
 -- fidget
 require('fidget').setup{}
