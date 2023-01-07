04650dbe5018504266f3474f8b84688ea3c7ed85
sh0e1 <sh0e1@users.noreply.github.com>
2023-01-07 23:29:33 +0900

    :wastebasket: Remove: blank line

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -42,7 +42,6 @@ return require('packer').startup(function(use)
     run = function() vim.fn["mkdp#util#install"]() end,
   })
 
-
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
