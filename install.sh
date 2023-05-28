9224c8c804bea1ff4654fe75db1fa35621f55c01
sh0e1 <sh0e1@users.noreply.github.com>
2023-05-28 14:41:53 +0900

    :wrench: Fix: shellcheck and shfmt

--- install.sh
+++ install.sh
@@ -2,23 +2,25 @@
 
 set -e
 
-dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+dir=$(
+    cd "$(dirname "${BASH_SOURCE:-$0}")"
+    pwd
+)
 
 # ${dir}/brew.sh
 # ${dir}/nvim.sh
 # ${dir}/tmux.sh
 
 for f in .??*; do
-  [[ "$f" == ".git" ]] && continue
-  [[ "$f" == ".DS_Store" ]] && continue
-  [[ "$f" == ".gitignore" ]] && continue
-  [[ "$f" == ".config" ]] && continue
-  ln -snfv ${dir}/${f} ${HOME}/${f}
+    [[ "$f" == ".git" ]] && continue
+    [[ "$f" == ".DS_Store" ]] && continue
+    [[ "$f" == ".gitignore" ]] && continue
+    [[ "$f" == ".config" ]] && continue
+    ln -snfv "${dir}/${f}" "${HOME}/${f}"
 done
 
 for f in .config/??*; do
-  ln -snfv ${dir}/${f} ${HOME}/${f}
+    ln -snfv "${dir}/${f}" "${HOME}/${f}"
 done
 
 echo "The dotfiles have been copied."
-
