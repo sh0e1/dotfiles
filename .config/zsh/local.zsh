7304c794518f6a200af477193fc5c89664c4cd35
sh0e1 <sh0e1@users.noreply.github.com>
2023-06-11 12:27:30 +0900

    :wrench: Fix: comment on local.zsh

--- .config/zsh/local.zsh
+++ .config/zsh/local.zsh
@@ -1,6 +1,6 @@
 # If local-specific settings are required, write them in this file.
 #
 # If you want to ignore the git diffs on this file, run the following command.
-# git update-index --skip-worktree $XDG_CONFIG_HOME/zsh/local.zsh
+# git update-index --skip-worktree .config/zsh/local.zsh
 
 # echo 'loaded local.zsh'
