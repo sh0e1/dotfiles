5aa7e866d21ee2cc68769f2942bc5018a625fe09
sh0e1 <sh0e1@users.noreply.github.com>
2023-12-30 16:51:13 +0900

    github-theme setup

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -56,6 +56,8 @@ return require('packer').startup(function(use)
   use({
     'projekt0n/github-nvim-theme',
     config = function()
+      require('github-theme').setup({})
+
       vim.cmd('colorscheme github_dark_dimmed')
     end
   })
