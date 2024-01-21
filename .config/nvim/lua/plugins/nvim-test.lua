aa64f841393d6754130e5533b36c19fcbdc36a95
sh0e1 <sh0e1@users.noreply.github.com>
2024-01-21 17:30:01 +0900

    refactor nvim-test

--- .config/nvim/lua/plugins/nvim-test.lua
+++ .config/nvim/lua/plugins/nvim-test.lua
@@ -1,3 +1,14 @@
 return {
   "klen/nvim-test",
+  keys = {
+    { '<Leader>t', '<Cmd>TestNearest<CR>' },
+    { '<Leader>T', '<Cmd>TestFile<CR>' },
+    { '<Leader>a', '<Cmd>TestSuite<CR>' },
+    { '<Leader>l', '<Cmd>TestLast<CR>' },
+    { '<Leader>g', '<Cmd>TestVisit<CR>' },
+  },
+  cmd = { "TestNearest", "TestFile", "TestSuite", "TestLast", "TestVisit", "TestEdit", "TestInfo" },
+  config = function()
+    require("nvim-test").setup()
+  end
 }
