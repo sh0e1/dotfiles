dcdee0c9e64d08b31c1cda6e5f76f0280ef67271
sh0e1 <sh0e1@users.noreply.github.com>
2024-01-27 16:38:39 +0900

    update vim-tmux-navigator configs

--- .config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ .config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,3 +1,15 @@
 return {
-  "christoomey/vim-tmux-navigator"
+  "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+  },
+  keys = {
+    { "<C-h>", "<Cmd><C-U>TmuxNavigateLeft<CR>" },
+    { "<C-j>", "<Cmd><C-U>TmuxNavigateDown<CR>" },
+    { "<C-k>", "<Cmd><C-U>TmuxNavigateUp<CR>" },
+    { "<C-l>", "<Cmd><C-U>TmuxNavigateRight<cr>" },
+  },
 }
