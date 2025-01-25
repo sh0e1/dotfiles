fe0f77a64b3839402407f475183f5fd16eff31d8
sh0e1 <sh0e1@users.noreply.github.com>
2025-01-25 15:33:06 +0900

    disable go.nvim dap debug

--- .config/nvim/lua/plugins/go.lua
+++ .config/nvim/lua/plugins/go.lua
@@ -1,5 +1,8 @@
 return {
   "ray-x/go.nvim",
+  dependencies = {
+    "ray-x/guihua.lua",
+  },
   ft = { "go", "gomod" },
   event = { "CmdlineEnter" },
   config = function()
@@ -9,6 +12,10 @@ return {
       gofmt = 'gopls',
       lsp_cfg = false,
       gopls_remote_auto = true,
+      dap_debug = false,
+      dap_debug_keymap = false,
+      dap_debug_gui = false,
+      dap_debug_vt = false,
       textobjects = true,
       test_runner = 'go',
       verbose_tests = true,
