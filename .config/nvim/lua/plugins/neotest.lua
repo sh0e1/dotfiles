b60721dd1278e60b3e4cbad3c8ac1d8b9b21340d
sh0e1 <sh0e1@users.noreply.github.com>
2025-01-26 16:35:32 +0900

    add neotest-go and setup neotest

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -4,5 +4,68 @@ return {
     "nvim-neotest/nvim-nio",
     "nvim-lua/plenary.nvim",
     "antoinemadec/FixCursorHold.nvim",
-  }
+    "nvim-neotest/neotest-go",
+  },
+  keys = {
+    {
+      "<Leader>tt",
+      function()
+        require("neotest").run.run()
+      end,
+      desc = "Run the nearest test"
+    },
+    {
+      "<Leader>T",
+      function()
+        require("neotest").run.run(vim.fn.expand("%"))
+      end,
+      desc = "Run the current file"
+    },
+    {
+      "<Leader>tl",
+      function()
+        require("neotest").run.run_last()
+      end,
+      desc = "Re-run the last test that was run."
+    },
+    {
+      "<Leader>td",
+      function() require("neotest").run.run({ strategy = "dap" }) end,
+      desc = "Debug the nearest test with dap"
+    },
+    {
+      "<Leader>ts",
+      function() require("neotest").summary.toggle() end,
+      desc = "Toggle the test summary window"
+    },
+    {
+      "<Leader>to",
+      function() require("neotest").output_panel.toggle() end,
+      desc = "Toggle the test output panel"
+    },
+  },
+  config = function()
+    require("neotest").setup({
+      adapters = {
+        require("neotest-go")({
+          args = { "-count=1", "-race" }
+        })
+      },
+      icons = {
+        child_indent = "|",
+        child_prefix = "|",
+        collapsed = "-",
+        expanded = "+",
+        failed = "X",
+        final_child_indent = " ",
+        final_child_prefix = "+",
+        non_collapsible = "-",
+        passed = "✓",
+        running = "…",
+        skipped = ">>",
+        unknown = "?",
+        watching = "∞",
+      },
+    })
+  end,
 }
