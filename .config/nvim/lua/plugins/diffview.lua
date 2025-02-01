fcefcd79f3de1d1419ef18c416c00b6a593283ff
sh0e1 <sh0e1@users.noreply.github.com>
2025-02-01 22:53:25 +0900

    set options

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -1,3 +1,11 @@
 return {
-  "sindrets/diffview.nvim"
+  "sindrets/diffview.nvim",
+  opts = {
+    use_icons = false,
+    signs = {
+      fold_closed = ">",
+      fold_open = "v",
+      done = "✓",
+    }
+  }
 }
