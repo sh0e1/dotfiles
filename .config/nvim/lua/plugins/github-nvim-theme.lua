1c63b03aad8baea02ee27ef809089e10a6def513
sh0e1 <sh0e1@users.noreply.github.com>
2025-02-15 19:14:34 +0900

    add styles options

--- .config/nvim/lua/plugins/github-nvim-theme.lua
+++ .config/nvim/lua/plugins/github-nvim-theme.lua
@@ -4,6 +4,20 @@ return {
   priority = 1000,
   config = function()
     require("github-theme").setup({
+      options = {
+        styles = {
+          comments = "italic",
+          functions = 'NONE',
+          keywords = 'NONE',
+          variables = 'NONE',
+          conditionals = 'NONE',
+          constants = 'NONE',
+          numbers = 'NONE',
+          operators = 'NONE',
+          strings = 'NONE',
+          types = 'NONE',
+        },
+      },
       groups = {
         github_dark_dimmed = {
           Directory = { fg = 'palette.blue' }
