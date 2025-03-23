1e3252581a3df9ed112d15bb0d43277f5ef64f82
sh0e1 <sh0e1@users.noreply.github.com>
2025-03-23 23:47:23 +0900

    Add mini.nvim and render-markdown.nvim plugins

---
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,11 @@
+return {
+  'MeanderingProgrammer/render-markdown.nvim',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'echasnovski/mini.nvim',
+  },
+  ft = { 'markdown', 'copilot-chat' },
+  opts = {
+    completions = { lsp = { enabled = true } },
+  },
+}
